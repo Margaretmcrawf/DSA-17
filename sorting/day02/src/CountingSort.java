b6fd60a1de3346dcb7ce3281b76f194d2c82dc7f
margaretmcrawf <margaretmcrawf@gmail.com>
2017-03-04 14:28:07 -0500

    saving counting sort stuff?

--- sorting/day02/src/CountingSort.java
+++ sorting/day02/src/CountingSort.java
@@ -1,11 +1,35 @@
+import javax.swing.*;
+
 public class CountingSort {
 
     /**
      * Use counting sort to sort positive integer array A.
-     * Runtime: TODO
+     * Runtime: k + n
      */
     static void countingSort(int[] A) {
-        // TODO
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+
+        int[] buckets = new int[max + 1];
+        for (int i = 0; i < A.length; i++) {
+             buckets[A[i]] = buckets[A[i]] + 1;
+        }
+
+        int index = 0;
+        int bucket = 0;
+        while (index < A.length) {
+            if (buckets[bucket] != 0) {
+                for (int j = 0; j < buckets[bucket]; j++) {
+                    A[index] = bucket;
+                    index++;
+                }
+            }
+            bucket++;
+        }
     }
 
 }
--- sorting/day02/src/Problems.java
+++ sorting/day02/src/Problems.java
@@ -1,7 +1,17 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
         // TODO
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] + 100;
+        }
+        RadixSort rs = new RadixSort();
+        rs.radixSort(A, 10);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] - 100;
+        }
     }
 
     /**
@@ -11,22 +21,36 @@ public class Problems {
     private static int getNthCharacter(String s, int n) {
         return s.charAt(s.length() - 1 - n) - 'a';
     }
-
-
     /**
      * Use counting sort to sort the String array according to a character
      *
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        int b = 26;
+        LinkedList<String>[] L = new LinkedList[b];
+        for (int i = 0; i < b; i++)
+            L[i] = new LinkedList<>();
+        for (String i : A) {
+            int digit = getNthCharacter(i, n);
+            L[digit].add(i);
+        }
+        int j = 0; // index in A to place numbers
+        for (LinkedList<String> list : L) {
+            for (String i : list) {
+                A[j] = i;
+                j++;
+            }
+        }
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i = 0; i < stringLength; i++) {
+            countingSortByCharacter(S, i);
+        }
     }
 
 }
